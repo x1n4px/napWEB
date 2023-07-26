40d6edeaae004d6ff1b8bed497589a41fcbb8201
x1n4px <silosabia@gmail.com>
2023-07-26 13:15:02 +0200

    act

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  showMenu: boolean = false;
+  selecction: number = 1;
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
 }
